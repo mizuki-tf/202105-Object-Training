1428bc47ef21c18d5bc44b9136e38d9fbdf1ce73
mizuki-tf <mi_matsui@uluru.jp>
2021-05-26 12:36:38 +0900

    b.作成したCounterクラスに機能を追加

--- class_counter.js
+++ class_counter.js
@@ -3,14 +3,24 @@ class Counter {
     this.count = 0;
   }
 
-  // カウント加算
+  // カウント増加
   up() {
     this.count += 1;
+  }
+
+  // カウント減少
+  down() {
+    this.count -= 1;
   }
 
   // countのゲッター
   getValue() {
     return this.count;
+  }
+
+  // 現在のカウントが0にリセットされる
+  resetValue() {
+    return this.count = 0;
   }
 }
 
@@ -19,3 +29,6 @@ counter.up();
 console.log(counter.getValue()); // => 1と表示される
 counter.up();
 console.log(counter.getValue()); // => 2と表示される
+counter.down();
+console.log(counter.getValue()); // => 1と表示される
+console.log(counter.resetValue()); // => 0と表示される
